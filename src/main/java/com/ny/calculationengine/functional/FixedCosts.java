c06262272245581768b6bf7adf27b7bf5100dd55
NY <n33raj18@gmail.com>
2017-11-12 20:45:48 +0530

    Redo the calculationengine using functional style
    -Use a FunctionalInterface to implement the Strategy pattern which we used for our type hierarchy before

---
+++ src/main/java/com/ny/calculationengine/functional/FixedCosts.java
@@ -0,0 +1,17 @@
+package com.ny.calculationengine.functional;
+
+public class FixedCosts implements QuantityOfInterest {
+    private final FunctionOverTime valueFunction;
+
+    public FixedCosts(FunctionOverTime valueFunction) {
+        this.valueFunction = valueFunction;
+    }
+
+    public String getName() {
+        return "Fixed Costs";
+    }
+
+    public double valueAt(int time) {
+        return valueFunction.valueAt(time);
+    }
+}
---
+++ src/main/java/com/ny/calculationengine/functional/IncrementalCosts.java
@@ -0,0 +1,17 @@
+package com.ny.calculationengine.functional;
+
+public class IncrementalCosts implements QuantityOfInterest {
+    private final FunctionOverTime valueFunction;
+
+    public IncrementalCosts(FunctionOverTime valueFunction) {
+        this.valueFunction = valueFunction;
+    }
+
+    public String getName() {
+        return "Incremental Costs";
+    }
+
+    public double valueAt(int time) {
+        return valueFunction.valueAt(time);
+    }
+}
