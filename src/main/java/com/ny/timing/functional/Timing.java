9dc255e5d8e457be980ec7d709d60ff5c6d785fe
NY <n33raj18@gmail.com>
2017-11-13 16:51:09 +0530

    Made the supplier generic

--- src/main/java/com/ny/timing/functional/Timing.java
+++ src/main/java/com/ny/timing/functional/Timing.java
@@ -7,10 +7,10 @@ import java.util.function.Supplier;
 
 public class Timing {
 
-    public static Double timed(String description, Supplier<Double> supplier, Consumer<String> consumer){
+    public static <T> T timed(String description, Supplier<T> supplier, Consumer<String> consumer){
 
         LocalTime startTime = LocalTime.now();
-        Double value = supplier.get();
+        T value = supplier.get();
         LocalTime endTime = LocalTime.now();
         consumer.accept("Time taken to get value from "
                 + description + ": " + ChronoUnit.SECONDS.between(startTime, endTime) + " seconds");
